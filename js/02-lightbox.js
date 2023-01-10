1c45299a1786005600513bba25e0e27588a8368e
YuliaKhyzhuk <anastezy1@gmail.com>
2023-01-10 07:55:39 +0200

    Завдання 2 частково виконано

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryListUlEl = document.querySelector(".gallery");
+
+const makeGalleryCard = ({ preview, original, description } = {}) => {
+  return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+};
+
+const galleryCardsAll = galleryItems.map((el) => makeGalleryCard(el)).join("");
+galleryListUlEl.insertAdjacentHTML("beforeend", galleryCardsAll);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  /* options */
+});
+gallery.captionPosition = "bottom";
 
 console.log(galleryItems);
